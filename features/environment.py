a08ccbe0cf679d7aed4140aa6f5c221c57c81ff6
DangCoop <105824586+DangCoop@users.noreply.github.com>
2024-08-30 22:29:08 -0400

    Upgrade my Test Case 2 with HEADLESS MODE and FIREFOX Browser. Everything WORKS

--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+
 from app.application import Application
+
 
 def browser_init(context):
     """
@@ -11,6 +15,20 @@ def browser_init(context):
     driver_path = ChromeDriverManager().install()
     service = Service(driver_path)
     context.driver = webdriver.Chrome(service=service)
+
+    ### HEADLESS MODE ####
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    #)
+
+    ###FIREFOX MODE###
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
